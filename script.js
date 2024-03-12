0a0d8b8e0d01ece578711a2db0933145a42c8bc7
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2024-03-12 09:41:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class Room {
         this.name = name;
         this.type = type;
         this.areaCalculator = areaCalculator;
+        this.form = this.getForm();
     }
 
     getArea() {
@@ -14,18 +15,37 @@ class Room {
     }
 
     getDescription() {
-        return this.description || `Назва: ${this.name}, Тип: ${this.type}, Площа: ${this.getArea()} м²`;
+        if (this.areaCalculator instanceof CircularRoom) {
+            return this.description || `Опис: ${this.name}, Тип: ${this.type}, Форма: ${this.form}, Радіус: ${this.areaCalculator.radius}, Площа: ${this.getArea()} м²`;
+        } else if (this.areaCalculator instanceof RectangularRoom) {
+            return this.description || `Опис: ${this.name}, Тип: ${this.type}, Форма: ${this.form}, Ширина: ${this.areaCalculator.width}, Висота: ${this.areaCalculator.height}, Площа: ${this.getArea()} м²`;
+        } else {
+            return this.description || `Опис: ${this.name}, Тип: ${this.type}, Форма: ${this.form}, Ширина: ${this.areaCalculator.base}, Висота: ${this.areaCalculator.height}, Площа: ${this.getArea()} м²`;
+        }
+    }
+
+    getForm() {
+        if (this.areaCalculator instanceof RectangularRoom) {
+            return 'Прямокутна';
+        } else if (this.areaCalculator instanceof TriangularRoom) {
+            return 'Трикутна';
+        } else if (this.areaCalculator instanceof CircularRoom) {
+            return 'Кругла';
+        } else {
+            return 'Не відома';
+        }
     }
 }
 
+
 class RectangularRoom {
-    constructor(length, width) {
-        this.length = length > 0 ? length : 0;
+    constructor(width, height) {
         this.width = width > 0 ? width : 0;
+        this.height = height > 0 ? height : 0;
     }
 
     calculateArea() {
-        return this.length * this.width;
+        return this.width * this.height;
     }
 }
 
@@ -83,9 +103,9 @@ class House {
 
     getRoomsCountByForm() {
         const countByForm = {};
-        
+
         this.rooms.forEach(room => {
-            const form = room.areaCalculator.constructor.name;
+            const form = room.form;
             countByForm[form] = (countByForm[form] || 0) + 1;
         });
 
@@ -93,12 +113,12 @@ class House {
     }
 
     getRoomsCountByType() {
-        const countByType = { };
+        const countByType = {};
 
         this.rooms.forEach(room => {
             countByType[room.type] = (countByType[room.type] || 0) + 1;
         });
-        
+
         return countByType;
     }
 
@@ -111,11 +131,10 @@ class House {
     }
 }
 
-// Приклад використання
 const house = new House();
 
 const livingRoom = new Room('Вітальня кімната', 'Вітальня', new RectangularRoom(5, 6));
-const bedroom = new Room('Спальня', 'Вітальня', new RectangularRoom(4, 4));
+const bedroom = new Room('Спальня', 'Вітальня', new RectangularRoom(4, 3));
 const kitchen = new Room('Кухня', 'Підсобне приміщення', new RectangularRoom(3, 3));
 const bathroom = new Room('Душова кімната', 'Підсобне приміщення', new RectangularRoom(2, 2));
 const triangularRoom = new Room('Трикутна кімната', 'Вітальня', new TriangularRoom(4, 5));
@@ -129,14 +148,13 @@ house.addRoom(triangularRoom);
 house.addRoom(circularRoom);
 
 console.log('Загальна площа житлових кімнат:', house.getTotalLivingArea(), 'м²');
-console.log('Площалоща всіх кімнат:', house.getTotalArea(), 'м²');
+console.log('Площа всіх кімнат:', house.getTotalArea(), 'м²');
 console.log('Перелік кімнат:', house.getRoomsList());
 console.log('Кількість кімнат кожної Форми:', house.getRoomsCountByForm());
 console.log('Кількість кімнат кожного Типу:', house.getRoomsCountByType());
 console.log('Використано Газу:', house.getGasUsage(), 'м³');
 console.log('Використано Води:', house.getWaterUsage(), 'літрів');
 
-// Редагування кімнати
 const newLivingRoom = new Room('Вітальня кімната', 'Вітальня', new RectangularRoom(6, 6));
 house.editRoom('Вітальня кімната', newLivingRoom);
 console.log('Список кімнат після редагування:', house.getRoomsList());
